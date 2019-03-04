a5a7fec30179f28e6e294461842593a86e6b35fd
Jack Morton <morton.jack@protonmail.com>
2019-03-05 03:45:20 +1030

    Added "rating" feature

--- js/script.js
+++ js/script.js
@@ -11,34 +11,39 @@ const quotes = [
     source: "Robert F. Kennedy",
     // citation: "Day of Affirmation Speech",
     year: "1966",
-    tag: "Failure"
+    tag: "Failure",
+    rating: "9/10"
   },
   {
     quote: "Live in the sunshine, swim the sea, drink the wild air",
     source: "Ralph Waldo Emerson",
     citation: "Merlin's Song Poem",
-    tag: "Joy"
+    tag: "Joy",
+    rating: "6/10"
   },
   {
     quote: "There is no such thing as luck",
     source: "Obi-Wan Kenobi",
     citation: "Star Wars: Episode IV- A New Hope",
     year: "1977",
-    tag: "Effort"
+    tag: "Effort",
+    rating: "7/10"
   },
   {
     quote: "A happy man is too satisfied with the present to think too much about the future.",
     source: "Einstein",
     citation: "School essay entitled \"My Future Plans\"",
     year: "1896",
-    tag: "Joy"
+    tag: "Joy",
+    rating: "8/10"
   },
   {
     quote: "I have not failed. I've just found 10,000 ways that won't work.",
     source: "Thomas Edison",
     /* citation: "Edison: His Life and Inventions",
     year: "1910" */
-    tag: "Failure"
+    tag: "Failure",
+    rating: "8/10"
   },
 ];
 
@@ -63,6 +68,9 @@ function printQuote() {
   }
   if (randQuote.tag) {
     htmlString += '</span><span class="tag">Tagged: ' + randQuote.tag + '</span>';
+  }
+  if (randQuote.rating) {
+    htmlString += '</span><span class="rating">' + randQuote.rating + ' people liked this quote</span>';
   }
   htmlString += '</p>';
 
